ab7e9e31fd62bdf115e0f063e6e794ade8165e84
DigiPie <evan.it.sg@gmail.com>
2019-11-04 21:21:26 +0800

    Update build.sh

--- app/build.sh
+++ app/build.sh
@@ -4,5 +4,6 @@
 # Ensure that there is a '.env' file in the same directory as 'package.json'
 # which should contain the environmental variable PUBLIC_URL.
 # Example: PUBLIC_URL= https://www.evantay.com/tech/react-node-docker/
+npm i
 npm run build
 docker build -t react-node-docker --no-cache .
