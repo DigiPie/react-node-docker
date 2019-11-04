0773384419bd594c328ea36e3fb0ac72dc91fe54
DigiPie <evan.it.sg@gmail.com>
2019-11-04 21:17:54 +0800

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
+import { IconContext } from 'react-icons';
+import { FaGithub, FaReact, FaNodeJs, FaDocker } from 'react-icons/fa';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <a href='https://github.com/DigiPie/react-node-docker' color='white' 
+            className='App-link' title="View DigiPie/React-Node-Docker on Github">
+          <h1><FaGithub /> DigiPie/<FaReact />-<FaNodeJs />-<FaDocker /></h1>
+        </a>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Running at: {process.env.PUBLIC_URL}
+          {process.env.PUBLIC_URL ? `Running at ${process.env.PUBLIC_URL}` 
+              : 'process.env.PUBLIC_URL is undefined. Set it if you are'
+                  + ' deploying from a sub-directory or sub-domain.'}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+        </p>
       </header>
     </div>
   );
