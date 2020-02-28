7912969e3c0a2c5592bd4cbd8bdb2997a6b7cc2c
DigiPie <evan.it.sg@gmail.com>
2020-02-28 13:56:07 +0800

    Update v0.1.1

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # react-node-docker
-An example for serving a create-react-app using NodeJS in a Docker container.
+A skeleton boiler-plate example for serving a create-react-app using NodeJS in a Docker container. Comes with `react-bootstrap`.
+
+[View live demo](https://evantay.com/tech/react-node-docker/)
+
+![Alt text](img/screenshot.png?raw=true "Screenshot")
+
+## How to use
+1. Change the directory to _app/_: `cd app`.
+2. Edit and execute: `build.sh`.
+3. Edit and execute: `run.sh`.
--- app/src/App.js
+++ app/src/App.js
@@ -1,48 +1,59 @@
-import React from 'react';
-import { FaGithub, FaReact, FaNodeJs, FaDocker } from 'react-icons/fa';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { FaGithub, FaReact, FaNodeJs, FaDocker } from "react-icons/fa";
+import { Button, ButtonGroup } from "react-bootstrap";
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <a href='https://github.com/DigiPie/react-node-docker' color='white' 
-            className='App-link' title="View DigiPie/React-Node-Docker on Github">
-          <h1><FaGithub /> DigiPie/<FaReact />-<FaNodeJs />-<FaDocker /></h1>
+    <div className="App">
+      <header className="App-header">
+        <a
+          href="https://github.com/DigiPie/react-node-docker"
+          color="white"
+          className="App-link"
+          title="View DigiPie/React-Node-Docker on Github"
+        >
+          <h1>
+            <FaGithub />-<FaReact />-<FaNodeJs />-<FaDocker />
+          </h1>
         </a>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <code>process.env.PUBLIC_URL=
-          {process.env.PUBLIC_URL ? `${process.env.PUBLIC_URL}` : 'undefined'}
-          </code>
-          <br />
-          Set <code>process.env.PUBLIC_URL</code> if you are deploying from a 
-          sub-directory or a sub-domain.
-          <br />
-          You can do so by adding a <code>.env</code> file containing 
-          <code> PUBLIC_URL</code> in the same folder as <code>package.json</code>.
-        </p>
-        <p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-          <span> | </span>
-          <a
-            className='App-link'
-            href='https://github.com/DigiPie/react-node-docker'
-          >
-            View source code
-          </a>
-        </p>
+        <h2>A React-Node-Docker boilerplate</h2>
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-text">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>
+            <code>
+              process.env.PUBLIC_URL=
+              {process.env.PUBLIC_URL
+                ? `${process.env.PUBLIC_URL}`
+                : "undefined"}
+            </code>
+            <br />
+            Set <code>process.env.PUBLIC_URL</code> if you are deploying from a
+            sub-directory or a sub-domain.
+            <br />
+            You can do so by adding a <code>.env</code> file containing
+            <code> PUBLIC_URL</code> in the same folder as{" "}
+            <code>package.json</code>.
+          </p>
+          <p>
+            <ButtonGroup>
+              <Button
+                variant="light"
+                href="https://github.com/DigiPie/react-node-docker"
+              >
+                Source Code
+              </Button>
+              <Button variant="dark" href="https://reactjs.org">
+                Learn React
+              </Button>
+            </ButtonGroup>
+          </p>
+        </div>
       </header>
     </div>
   );
